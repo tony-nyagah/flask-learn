50ce9206842ed8ce345723619c2d309031ec2ef4
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-06-25 14:04:45 +0300

    server post route

--- flask-vue-crud/backend/app.py
+++ flask-vue-crud/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # instantiate the app
@@ -26,9 +26,24 @@ BOOKS = [
 ]
 
 
-@app.route("/books", methods=["GET"])
+@app.route("/books", methods=["GET", "POST"])
 def all_books():
-    return jsonify({"status": "success", "books": BOOKS})
+    response_object = {"status": "success"}
+
+    if request.method == "POST":
+        post_data = request.get_json()
+        BOOKS.append(
+            {
+                "title": post_data.get("title"),
+                "author": post_data.get("author"),
+                "read": post_data.get("read"),
+            }
+        )
+        response_object["message"] = "Book added!"
+    else:
+        response_object["books"] = BOOKS
+
+    return jsonify(response_object)
 
 
 if __name__ == "__main__":

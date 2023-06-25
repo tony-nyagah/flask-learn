a7298e5579ea913d7bc7b665eacbce0bf6e0effb
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-06-25 09:00:02 +0300

    Initial Vue and Flask setup

---
+++ flask-vue-crud/frontend/src/components/Ping.vue
@@ -0,0 +1,23 @@
+<script setup lang="js">
+import { ref } from 'vue';
+import axios from 'axios';
+
+const message = ref("Hello");
+
+function getMessage() {
+    const path = "http://localhost:5000/ping";
+    axios.get(path).then((response) => {
+        message.value = response.data;
+    }).catch((error) => {
+        message.value = error;
+    });
+}
+</script>
+
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <button @click="getMessage">Get Message!</button>
+    </div>
+</template>
+
